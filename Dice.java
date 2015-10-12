c1aaf95d7a37e022ec77864f5841db7f0510acfb
bjohnson1986 <bjohnson@hotmail.com.au>
2015-10-12 20:11:38 +1100

    Bug 4 resolution had redundant code, this has been simplified and retested.

--- Dice.java
+++ Dice.java
@@ -11,9 +11,8 @@ public class Dice {
 	}
 
 	public DiceValue roll() {
-		DiceValue d = DiceValue.getRandom();
-		value = d;
-		return d;
+		value = DiceValue.getRandom();
+		return value;
 	}		
 	
 	public String toString() {
